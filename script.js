3fdf18644a2583cd3776307bb29459ce97bf3d72
Marcos Oliveira <marcosoliveira253.mo@gmail.com>
2023-06-25 16:34:21 -0300

    atualização
    Fiz uma atualização na background do body pelo css,  acrescentei um hashtag no link de voltar ao topo, e acrescentei uma função para fechar o menu lateral após clicado em um link.

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const menuIcon = document.querySelector('.menu-icon');
-var ul = document.querySelector('.ul');
+var menu = document.querySelector('.ul');
 
 menuIcon.addEventListener('click', ()=>{
     
-    if (ul.classList.contains('ativo')) {
-        ul.classList.remove('ativo');
+    if (menu.classList.contains('ativo')) {
+        menu.classList.remove('ativo');
         document.querySelector('.menu-icon img').src = './img/menu.png';
     } else {
-        ul.classList.add('ativo');
+        menu.classList.add('ativo');
         document.querySelector('.menu-icon img').src = './img/close.png';
     }
-})
+});
 
+/* Evento de click para cada link do menu para remover a classe active do menu ao clicar em um link. */
+
+const menuLinks = document.querySelectorAll('.ul li a');
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('ativo');
+        document.querySelector('.menu-icon img').src = './img/menu.png';
+    });
+});
